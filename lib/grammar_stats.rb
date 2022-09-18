b805607f179322d504d6125e07e0fc1b726a10da
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-09-18 07:16:47 +0100

    completed challenge

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -7,11 +7,20 @@ class GrammarStats
     def check(text) # text is a string
       # Returns true or false depending on whether the text begins with a capital
       # letter and ends with a sentence-ending punctuation mark.
-      if text.match? [/A-Z/]
+      if text.match?(/^[A-Z].*\W$/)
+        @pass += 1
+        true
+      else
+        @fail += 1
+        false
+      end
     end
-  
+
     def percentage_good
       # Returns as an integer the percentage of texts checked so far that passed
       # the check defined in the `check` method. The number 55 represents 55%.
+      total = @pass + @fail
+      @pass / total.to_f * 100
     end
 end
+
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,9 +1,44 @@
 require 'grammar_stats'
 
-RSpec.describe do
-    it "Checks if text begins with a capital letter" do
-    grammar = GrammarStats.new
-    expect(grammar).to eq "Hello my name is Aamir"
+RSpec.describe GrammarStats do
+		describe "#check(text)" do
+    it "Checks if text begins with a capital letter and returns true" do
+        grammar_check = GrammarStats.new
+        result = grammar_check.check("Hello my name is Aamir.")
+    expect(result).to eq true
     end
-end
 
+		describe "#check(text)" do
+			it "returns false if text begins with a lowercase letter and a full stop" do
+					grammar_check = GrammarStats.new
+					result = grammar_check.check("hello my name is Aamir.")
+			expect(result).to eq false
+			end
+
+			describe "#check(text)" do
+				it "returns false if text begins with a capital letter but no full stop at the end" do
+						grammar_check = GrammarStats.new
+						result = grammar_check.check("Hello my name is Aamir")
+				expect(result).to eq false
+				end
+
+			describe "#check(text)" do
+				it "returns false if text has no lowercase letter at the start or full stop at the end" do
+						grammar_check = GrammarStats.new
+						result = grammar_check.check("hello my name is aamir")
+				expect(result).to eq false
+				end
+		end
+	end
+
+	describe "#percentage_good" do
+    it "gives the percentage of correct grammar checks" do
+      grammar_check = GrammarStats.new
+      grammar_check.check("Hello, I am Aamir.")
+      grammar_check.check("Hello, I am aamir")
+      expect(grammar_check.percentage_good).to eq 50
+		end
+end
+end
+end
+end
